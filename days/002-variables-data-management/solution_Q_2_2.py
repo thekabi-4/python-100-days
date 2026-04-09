e6989c784fba8ed4f9016785681b5f1b373e7176
kabilesh <klshktrh@gmail.com>
2026-04-10 00:03:33 +0530

    Complete Day 2: Variables & Data Management

---
+++ days/002-variables-data-management/solution_Q_2_2.py
@@ -0,0 +1,9 @@
+# Get inputs from user
+base_price = float(input())
+tax_rate = float(input())
+print(round(base_price+(base_price*tax_rate),2))
+# Calculate final price
+# Step 1: Use input() to get base_price (float) and tax_rate (float).
+# Step 2: Print base_price + (base_price * tax_rate), rounded to 2 decimals.
+# TODO: Add your solution logic here
+# Remember to print the required output
